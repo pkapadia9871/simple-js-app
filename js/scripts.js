fdeebcccc7b5fcc93539992149ea9e5694316882
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2023-08-03 10:06:26 -0500

    Added for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    { name: 'Ivysaur', height: 10, type: ['grass', 'poison']},
-    { name: 'Venusaur', height: 4, type: ['grass', 'poison']},
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    { name: 'Venusaur', height: 0.4, types: ['grass', 'poison']},
   ];
+
+  for (let i = 0; i <= pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    if (pokemonList[i].height >= 1.0)
+    {
+      document.write(' - Wow, thats big!')  
+    }
+    document.write('<br>');
+  }
