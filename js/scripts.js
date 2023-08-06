509035a1aae7ffe45b6480f6ae42232c24bfb60e
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2023-08-06 16:05:46 -0500

    Added bootstrap components.

--- js/scripts.js
+++ js/scripts.js
@@ -6,50 +6,52 @@ let pokemonRepository = (function () {
     function add(item) {
       pokemonList.push(item);
     }
+
     function getAll() {
       return pokemonList;
     }
+    
     function showDetails(pokemon) {
       loadDetails(pokemon).then(function () {
         showModal(pokemon)
       });
     }
+
     function showModal(pokemon) {
-      let modalContainer = document.querySelector('#modal-container');
+      let modalContainer = document.querySelector('.modal-body');
     
       modalContainer.innerHTML = '';
       let modal = document.createElement('div');
-      modal.classList.add('modal');
-    
-      let closeButtonElement = document.createElement('button');
-      closeButtonElement.classList.add('modal-close');
-      closeButtonElement.innerText = 'Close';
-      closeButtonElement.addEventListener('click', function () {
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.remove('is-visible');
-      })
+      /*modal.classList.add('modal-body');*/
+
       let titleElement = document.createElement('h1');
       titleElement.innerText = 'name: ' + pokemon.name;
       let contentElement = document.createElement('p');
       contentElement.innerText = 'height: ' + pokemon.height;
-    
       let myImage = document.createElement('img');
-    
       myImage.src = pokemon.imageUrl;
     
-      modal.appendChild(closeButtonElement);
       modal.appendChild(titleElement);
       modal.appendChild(contentElement);
       modal.appendChild(myImage);
       modalContainer.appendChild(modal);
-      modalContainer.classList.add('is-visible');
+      modalContainer.classList.add('visible');
+
     }
+
     function addListItem(pokemon) {
       let element = document.querySelector('ul')
       let listItem = document.createElement('li')
       let button = document.createElement('button')
+      listItem.classList.add('list-group-item')
       button.innerText = pokemon.name
-      button.classList.add('my-class')
+      button.classList.add('btn')
+      /*data-toggle, data-target*/
+      $('[data-toggle="modal"]').on('click', function(){
+        let targetSelector = $(this).attr('data-target');
+        $(targetSelector).modal('show'); // Bootstrap’s own function to make the modal appear
+      });
+      /*end of code*/
       listItem.appendChild(button)
       element.appendChild(listItem)
       button.addEventListener('click', function (event) {
@@ -57,6 +59,7 @@ let pokemonRepository = (function () {
         console.log(event);
       });
     }
+
     function loadList() {
       return fetch(apiUrl).then(function (response) {
         return response.json();
@@ -72,6 +75,7 @@ let pokemonRepository = (function () {
         console.error(e);
       })
     }
+
     function loadDetails(item) {
       let url = item.detailsUrl;
       return fetch(url).then(function (response) {
@@ -85,6 +89,7 @@ let pokemonRepository = (function () {
         console.error(e);
       });
     }
+
     return {
       add: add,
       getAll: getAll,
